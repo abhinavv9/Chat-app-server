876ed41ab5ac50ea0563345a3e53dec9d394b7cb
abhinavv9 <abhinavvrm14@gmail.com>
2025-03-06 05:28:23 +0530

    refactor: replace config.json with config.js to utilize environment variables for database configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "url": "postgresql://postgres:WjfgRxxcb4ib4BCL@db.nsrzegwsabvqegguzbiw.supabase.co:5432/postgres",
+    "url": process.env.DATABASE_URL,
     "dialect": "mysql"
   },
   "test": {
@@ -11,10 +13,7 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "url": process.env.DATABASE_URL,
     "dialect": "mysql"
   }
 }
